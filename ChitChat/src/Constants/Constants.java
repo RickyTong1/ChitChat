461289bcaa53dbe8d9516dbc979e5ef6bf846705
RickyTong1 <37036119+RickyTong1@users.noreply.github.com>
2018-09-28 20:52:20 +0800

    Add files via upload

--- ChitChat/src/Constants/Constants.java
+++ ChitChat/src/Constants/Constants.java
@@ -1,5 +1,14 @@
 package Constants;
 
 public interface Constants {
-
+	public static final int USER_PROFILE_IMAGE_WIDTH = 30;//主页面用户的头像直径
+	public static final int CONTACTS_PROFILE_IMAFE_WIDTH = 20;//联系人的头像直径
+	public static final int CONTACTS_PANEL_HEIGHT = 50;//联系人框的高度
+	public static final int CONTACTS_PANEL_WIDTH = 200;//联系人框的宽度;
+	public static final int MAIN_WINDOW_HEIGHT = 800;//主页面高度
+	public static final int MAIN_WINDOW_WIDTH = 800;//主页面宽度
+	public static final int MESSAGE_PANEL_HEIGHT = 70;//消息框高度
+	public static final int MESSAGE_PANEL_WIDTH = 300;//消息框宽度
+	public static final int MESSAGE_PROFILE_IMAGE_WIDTH = 20;//消息框头像直径
+	
 }
