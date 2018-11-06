c8c2a4f9d655a7f4dc4747d387fe9d3474a6b177
RickyTong <1343053743@qq.com>
2018-11-06 19:52:02 +0800

    delete

--- src/Constants/Initialization.java
+++
@@ -1,8 +0,0 @@
-package Constants;
-
-public interface Initialization {
-	public static final int CONTS = 1;
-	public static final int MESSAGE = 2;
-	public static final int CONTACTS =3;
-	//public static final int 
-}
