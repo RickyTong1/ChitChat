a596ca6621986ad8b06c352156625c7cd85b075c
RickyTong <1343053743@qq.com>
2018-11-21 20:49:33 +0800

    bug fixes.

--- ChitChat/src/CComponents/MessageBlob.java
+++ ChitChat/src/CComponents/MessageBlob.java
@@ -3,7 +3,6 @@ package CComponents;
 import java.awt.Image;
 import java.io.Serializable;
 
-import Client.Contacts;
 
 public class MessageBlob implements Serializable{
 	/*
@@ -17,21 +16,44 @@ public class MessageBlob implements Serializable{
 	* 建议通过调用MessageBlobOperator类中相应的静态方法提取MessageBlob中的信息.
 	*
 	*/
-	int id;
+	int senderID;
+	int targetID;
+	
 	int onlineState;
 	int isRead;//是否已读
 	
+	String key;//密码
 	String nickname;
 	String remark;//备注
 	String style;//个性签名
 	String gender;//性别
+	String email;
+	String birth;
+	String phoneNum;//手机号
+	
 	String[] messagesUnread;//未读消息
+	
+	
+	String senderIP;
+	String targetIP;
+	
 	
 	Image pic;
 
-	Contacts[] contactList;//好友列表
+	class Contacts{
+		String nickname;
+		int id;
+		String style;
+		String remark;
+		Image pic;
+	}
+	
+	Contacts[] contactslist;
 	
 	MessageBlobType type;
-	MessageBlobType answer;
+	MessageAnswerType answer;
 	long lastTimeSpeak;
+	
+	
 }
+
